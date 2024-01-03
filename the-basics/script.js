db7de969d624f4b276d6216f089cb2e789301457
fillip husebø <felipehusebo@gmail.com>
2024-01-03 12:27:54 +0100

    ok

---
+++ the-basics/script.js
@@ -0,0 +1,114 @@
+// // Variables
+// var myName = "Line";
+// console.log(myName);
+//
+// let age = 32;
+// console.log(age);
+//
+// const BRAND = "BMW";
+// console.log(BRAND);
+//
+//
+// // DATA TYPES
+// let greeting = "hello world";
+// console.log(typeof greeting);
+//
+// let isHappy = true;
+// console.log(isHappy);
+// console.log(typeof isHappy);
+//
+// let someVar;
+// console.log(typeof someVar);
+//
+// let emptyValue = null;
+// console.log(typeof emptyValue); //null
+//
+//
+// // Object
+// let person = {
+//     firstName:"Hesham",
+//     lastName: "El Masry",
+//     age: 31
+// };
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.age);
+//
+// let car = {
+//     make: "Volvo",
+//     year: 1992,
+//     condition: "used",
+//     price: 10000
+// };
+// console.log(car);
+// console.log(`i have a ${car.make} and it is ${car.year} and its ${car.condition} and the price is ${car.price}` );
+//
+//
+// //ARRAY
+// let colors = ["red", "green", "blue"]
+//
+// let random = [
+//     55566,
+//     "green",
+//     {name: "hesh", age: 31},
+//     ["boy", "girl", "whatever"]
+// ];
+// console.log(random[0]);
+// console.log(random[1]);
+// console.log(random[2]);
+// console.log(random[3]);
+//
+//
+// //TYPE CONVERSION
+// let year= "1992"; //string
+//
+// //from string to number
+// let convertedYear = parseInt(year);
+// console.log(convertedYear)
+//
+// //from number to string
+// let num = 123;
+// console.log(num.toString())
+//
+// let convertedNumber =num.toString()
+// console.log(convertedNumber)
+
+
+//task
+var city = "Paris";
+console.log(city);
+
+let age = 21;
+console.log(age);
+
+const birthYear = 2002;
+console.log(birthYear);
+
+
+let greeting = "Greetings World";
+console.log(typeof greeting);
+
+let quantity = 400;
+console.log(typeof quantity);
+
+let isJavaScriptFun = true;
+console.log(typeof isJavaScriptFun);
+
+let thisIsUndefined;
+console.log(typeof thisIsUndefined);
+
+let emptyValue = null;
+console.log(typeof emptyValue)
+
+
+let numString = "25";
+let num = parseInt(numString);
+console.log(num);
+
+let num2 = 50;
+let numString2 = num2.toString();
+console.log(numString2);
+
+let sum = num + num2;
+console.log(sum);
